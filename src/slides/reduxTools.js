d3e4dc33ba0b747b55a4f7550ab3220c7faac017
Michał Przyszczypkowski <mprzyszczypkowski@apptension.com>
2020-01-17 16:15:57 +0100

    Redux tools slides

--- src/slides/reduxTools.js
+++ src/slides/reduxTools.js
@@ -1,10 +1,30 @@
-import { Heading, Slide } from 'spectacle';
+import { Appear, CodePane, Heading, Slide, Text } from 'spectacle';
 import React from 'react';
 
 export const reduxTools = [
   (
     <Slide>
       <Heading>Redux tools</Heading>
+      <Heading style={{ textDecoration: 'line-through' }}>redux-sauce</Heading>
+
+      <CodePane>
+        {`const createAction = actionCreator('LOCALES');
+
+export const setLanguage = createAction('SET_LANGUAGE');`}
+      </CodePane>
+
+      <Appear elementNum={0}>
+        <Text>Usage</Text>
+        <CodePane>
+          {`dispatch(setLanguage('pl'));
+          
+setLanguage.toString() === 'LOCALES/SET_LANGUAGE';
+
+//in sagas
+takeLatest(setLanguage, handleSetLanguage)]
+          `}
+        </CodePane>
+      </Appear>
     </Slide>
-  )
+  ),
 ];
