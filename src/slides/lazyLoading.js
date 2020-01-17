80bf8e1bb71eb5382a10ae5547833e3b76a621f1
Michał Przyszczypkowski <mprzyszczypkowski@apptension.com>
2020-01-17 16:24:09 +0100

    Lazy loading slide

--- src/slides/lazyLoading.js
+++ src/slides/lazyLoading.js
@@ -1,10 +1,26 @@
-import { Heading, Slide } from 'spectacle';
+import { Appear, CodePane, Heading, Notes, Slide, Text } from 'spectacle';
 import React from 'react';
 
 export const lazyLoading = [
   (
     <Slide>
       <Heading>Lazy loading</Heading>
+      <Heading fontSize="h2">@loadable/component</Heading>
+
+      <Appear elementNum={0}>
+        <Text>1) Load component using asyncComponent helper</Text>
+        <CodePane>{`const Home = asyncComponent(() => import('./home'));`}</CodePane>
+      </Appear>
+
+      <Appear elementNum={1}>
+        <Text>2) Use like like regular component</Text>
+        <CodePane>{`<Home />`}</CodePane>
+      </Appear>
+
+      <Appear elementNum={2}>
+        <Text>3) Profit</Text>
+        <Text>It will be spit into separate chunk and only loaded when its actually rendered.</Text>
+      </Appear>
     </Slide>
   )
 ];
