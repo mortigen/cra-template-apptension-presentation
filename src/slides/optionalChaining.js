5999e2c866583b9af8db79b51bbdf3ab1f171c53
Michał Przyszczypkowski <mprzyszczypkowski@apptension.com>
2020-01-17 13:24:15 +0100

    Nullish example

--- src/slides/optionalChaining.js
+++ src/slides/optionalChaining.js
@@ -1,21 +1,53 @@
 import React from "react";
-import { Slide, Heading, Text, CodePane } from "spectacle";
+import { Slide, Heading, Text, CodePane, Appear } from "spectacle";
 
-const optionalChainingExample = `const street = user?.address?.street`;
+const chainingNow = `const street = user?.address?.street`;
 
-const oldWayExample = `const street = user && user.address && user.address.street
+const chainingThen = `const street = user && user.address && user.address.street
 
 // OR
 
 const street = R.path(['address', 'street'], user)`;
 
-export const optionalChainingSlide = (
-  <Slide>
-    <Heading>Optional chaining and Nullish Coalescing Operators</Heading>
+const nullishThen = `
+const fallbackToMinusOne = a => a || -1;
+fallbackToMinusOne(null); // -1
+fallbackToMinusOne(0); // -1
+fallbackToMinusOne(1); // 1
+`;
 
-    <Text>Optional chaining:</Text>
-    <CodePane>{optionalChainingExample}</CodePane>
-    <Text>Old way:</Text>
-    <CodePane>{oldWayExample}</CodePane>
-  </Slide>
-);
+const nullishNow = `
+const fallbackToMinusOne = a => a ?? -1;
+fallbackToMinusOne(null); // -1
+fallbackToMinusOne(0); // 0
+fallbackToMinusOne(1); // 1
+`;
+
+export const optionalChainingSlide = [
+    (
+      <Slide>
+          <Heading>Optional chaining</Heading>
+
+          <CodePane>{chainingThen}</CodePane>
+
+          <Appear elementNum={0}>
+            <Text>Becomes this:</Text>
+
+            <CodePane>{chainingNow}</CodePane>
+          </Appear>
+      </Slide>
+    ),
+    (
+      <Slide>
+          <Heading>Nullish Coalescing</Heading>
+
+          <CodePane>{nullishThen}</CodePane>
+
+          <Appear elementNum={0}>
+            <Text>Becomes this:</Text>
+
+            <CodePane>{nullishNow}</CodePane>
+          </Appear>
+      </Slide>
+    )
+];
