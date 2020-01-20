9139cbc2049c5d62d23be31a35f2b35cc151a633
Jakub Wiśniewski <jwisniewski@apptension.com>
2020-01-20 23:48:17 +0100

    other features

--- src/slides/fixedBugs.js
+++ src/slides/fixedBugs.js
@@ -3,13 +3,18 @@ import { Slide, Heading, Text, CodePane, UnorderedList, ListItem } from "spectac
 
 export const fixedBugs = (
   <Slide>
-    <Heading>Fixed bugs</Heading>
-
-    <Text>There has been some bug fixes and other improvements</Text>
+    <Heading>Fixed bugs and other improvements</Heading>
 
     <UnorderedList>
-        <ListItem>Unsupported browser in google search</ListItem>
+        <ListItem>Unsupported browser in google search fixed</ListItem>
         <ListItem>nock memory leak problem</ListItem>
+        <ListItem>react-helmet-async instead react-helmet (because of react warnings)</ListItem>
+        <ListItem>jest-styled-components</ListItem>
+        <ListItem>react-router 5 (hooks!)</ListItem>
+        <ListItem>styled-components 5</ListItem>
+        <ListItem>storybook knobs</ListItem>
+        <ListItem>hot reload supporting hooks</ListItem>
+        <ListItem>Fixed deprecated babel polyfill</ListItem>
     </UnorderedList>
 
   </Slide>
